f5f040460c33442d240886e9f3c89e6be4c5699f
Samuel Ramos Barroso <alu0100815146@ull.edu.es>
2018-09-26 14:31:00 +0100

    adding actions

---
+++ src/actions/productActions.js
@@ -0,0 +1,14 @@
+import {
+  FETCH_PRODUCTS_SUCCESS,
+  FETCH_PRODUCT_FAILURE,
+  FETCH_PRODUCT_SUCCESS,
+  FETCH_PRODUCT_FAILURE,
+  SAVE_PRODUCT_SUCCESS,
+  SAVE_PRODUCT_FAILURE
+} from 'types'
+
+export function fetchProductsSuccess (products) {
+  return {
+    
+  }
+}
